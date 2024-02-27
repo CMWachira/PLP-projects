3c98a89136496c41c1070caf1af79eec29a673fb
unknown <christine.wachirawc@gmail.com>
2024-02-27 09:22:03 +0300

    first commit

---
+++ user_input.py
@@ -0,0 +1,11 @@
+# Ask user for their name
+name = input("What is your name?")
+
+# Ask user for their age
+age = input("What is your age?")
+
+# Ask user for there location
+location = input("Where are you located at the moment?")
+
+#print of a personalized message to the user
+print("hello {}, you are {} years old and you live in {}. Welcome to PLP Academy.".format(name, age, location))
